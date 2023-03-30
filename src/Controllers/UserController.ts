e035c0950bc53f7b087dc2f716d4e6fd6184e550
nicsylvia <nicsylvia15f@gmail.com>
2023-03-30 16:50:39 -0700

    user registration endpoint all done

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -44,6 +44,42 @@ export const GetSingleUser = AsyncHandler(
   }
 );
 
+// Users Registration:
+export const UsersRegistration = AsyncHandler(
+  async (req: any, res: Response, next: NextFunction) => {
+    const { name, email, phoneNumber, username, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const findEmail = await UserModels.findOne({ email });
+
+    if (findEmail) {
+      next(
+        new AppError({
+          message: "User with this account already exists",
+          httpcode: HTTPCODES.FORBIDDEN,
+        })
+      );
+    }
+
+    const Users = await UserModels.create({
+      name,
+      email,
+      username,
+      phoneNumber: "234" + phoneNumber,
+      password: hashedPassword,
+      confirmPassword: hashedPassword,
+      status: "User",
+    });
+
+    return res.status(201).json({
+      message: "Successfully created User",
+      data: Users,
+    });
+  }
+);
+
 // //Getting All users
 // export const getUser = AsyncHandler(
 //   async (req: Request, res: Response, next: NextFunction) => {

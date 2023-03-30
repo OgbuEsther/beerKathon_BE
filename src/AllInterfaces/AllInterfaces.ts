76d89090a2999df857efb01ae8e3b8be3731d8fa
nicsylvia <nicsylvia15f@gmail.com>
2023-03-30 16:12:27 -0700

    saved match interfaces

---
+++ src/AllInterfaces/AllInterfaces.ts
@@ -0,0 +1,19 @@
+export interface iMatch {
+  startPlay: boolean;
+  stopPlay: boolean;
+
+  teamA: string;
+  teamB: string;
+
+  teamBOdds: number;
+  Odds: number;
+
+  teamAScore: number;
+  teamBScore: number;
+
+  dateTime: string;
+
+  scoreEntry: string;
+
+  predict: any[];
+}

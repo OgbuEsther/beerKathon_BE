3aea0b31e704ebd9c82af1d94082e781f394cf4e
nicsylvia <nicsylvia15f@gmail.com>
2023-03-30 22:01:26 -0700

    saved updated predictions

--- src/Routes/PredictRoutes.ts
+++ src/Routes/PredictRoutes.ts
@@ -1,21 +1,22 @@
 import express from "express";
 import {
-  createPrediction,
-  viewAllPredictions,
-  allPredictions,
-  predictionTable,
   userPredictionTable,
+  ViewAllPredictions,
+  CreatePrediction,
+  AllPredictions,
+  PredictionTable,
 } from "../Controllers/PredictController";
 
 const router = express.Router();
 
-router.route("/:userid/view-user-predictions").get(viewAllPredictions);
+router.route("/:userid/view-user-predictions").get(ViewAllPredictions);
+
 router.route("/:id/user-predictions").get(userPredictionTable);
 
-router.route("/:id/:ID/create-prediction").post(createPrediction);
+router.route("/:id/:ID/create-prediction").post(CreatePrediction);
 
-router.route("/prediction").get(allPredictions);
+router.route("/prediction").get(AllPredictions);
 
-router.route("/leader-table").get(predictionTable);
+router.route("/leader-table").get(PredictionTable);
 
 export default router;
